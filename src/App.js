43f10bb3c6e3a60aff11182e046e5a3a44e231af
Sreeman Tummala <super.tummala@gmail.com>
2024-10-09 01:33:11 -0500

    these are all the things I will do in my first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import About from './pages/About';
+import ContactForm from './components/ContactForm';
+import FeaturedProjects from './components/FeaturedProjects';
+import CareerTimeline from './components/CareerTimeline';
+import StatsCounter from './components/StatsCounter';
+import Services from './components/Services';
+import Testimonials from './components/Testimonials';
+import Blog from './components/Blog';
+import CTABanner from './components/CTABanner';
+import HeroBanner from './components/HeroBanner';
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* Navbar */}
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/blog">Blog</Link></li>
+          </ul>
+        </nav>
+
+        {/* Main Routes */}
+        <Routes>
+          <Route path="/" element={
+            <div>
+              <HeroBanner />
+              <FeaturedProjects />
+              <CareerTimeline />
+              <StatsCounter />
+              <Services />
+              <Testimonials />
+              <CTABanner />
+            </div>
+          } />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<ContactForm />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
