c73242f7ff6643ee486e970895b4681e03eedb51
Sreeman Tummala <super.tummala@gmail.com>
2024-10-09 19:20:33 -0500

    changes to home

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import './styles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Check if the app is hosted on GitHub Pages with the "/portfolio" path
+if (window.location.pathname === "/portfolio/") {
+  // Redirect to the main GitHub Pages URL
+  window.location.href = "https://SreemanTummala.github.io/";
+} else {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
